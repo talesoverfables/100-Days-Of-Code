36c77ba1622da4e7b7c5fb3c9a776872a67ceccc
Camomile <166503379+talesoverfables@users.noreply.github.com>
2025-05-12 23:01:29 +0530

    Create Day-16-LC-172-Factorial-Trailing-Zeros.java

---
+++ SWeek1/Day-16-LC-172-Factorial-Trailing-Zeros.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        if(n<5){
+            return 0;
+        }
+        while(n>=5){
+            n = n/5;
+            count = count + n;
+        }
+        return count;
+    }
+}
